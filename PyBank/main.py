78bfa89207e0c6b8537f48f176c5e5833a0ccd22
matthill <matthewhill60625@gmail.com>
2023-09-25 12:17:47 -0500

    This is the updated code for the python challenge

---
+++ PyBank/main.py
@@ -0,0 +1,74 @@
+import os
+import csv
+
+
+csvpath = os.path.join("Resources", "budget_data.csv")
+
+
+# #Veriables need to be defined
+total_months = 0
+total_profit_loss = 0
+previous_profit_loss = 0
+profit_loss_changes = []
+dates = []
+greatest_increase_date = ""
+greatest_increase_amount = 0
+greatest_decrease_date = ""
+greatest_decrease_amount = 0
+
+
+# #This will read the csv file 
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    header = next(csvreader)  # Skip the header row
+
+    # Iterate through the rows in the CSV file
+    for row in csvreader:
+        # Extract data from the current row
+        date = row[0]
+        profit_loss = int(row[1])
+
+        # Calculate total number of months and net total profit/loss
+        total_months += 1
+        total_profit_loss += profit_loss
+
+        # Calculate profit/loss changes and store them in a list
+        if total_months > 1:
+            profit_loss_change = profit_loss - previous_profit_loss
+            profit_loss_changes.append(profit_loss_change)
+            dates.append(date)
+
+            # Find the greatest increase and decrease in profits
+            if profit_loss_change > greatest_increase_amount:
+                greatest_increase_amount = profit_loss_change
+                greatest_increase_date = date
+            elif profit_loss_change < greatest_decrease_amount:
+                greatest_decrease_amount = profit_loss_change
+                greatest_decrease_date = date
+
+        # Update previous profit/loss for the next iteration
+        previous_profit_loss = profit_loss
+
+# Calculate the average of profit/loss changes
+average_change = sum(profit_loss_changes) / len(profit_loss_changes)
+
+# Print the results
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${total_profit_loss}")
+print(f"Average Change: ${average_change:.2f}")
+print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase_amount})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease_amount})")
+
+# Write the results to a text file
+with open('financial_analysis.txt', 'w') as textfile:
+    textfile.write("Financial Analysis\n")
+    textfile.write("----------------------------\n")
+    textfile.write(f"Total Months: {total_months}\n")
+    textfile.write(f"Total: ${total_profit_loss}\n")
+    textfile.write(f"Average Change: ${average_change:.2f}\n")
+    textfile.write(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase_amount})\n")
+    textfile.write(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease_amount})\n")
+
+print("Results have been written to 'financial_analysis.txt'")
---
+++ PyPoll/main.py
@@ -0,0 +1,61 @@
+import os
+import csv
+
+csvpath = os.path.join("Resources", "election_data.csv")
+
+#Veriables needed to be defined
+total_votes = 0
+candidates = {}
+
+#This will read the csv file 
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    header = next(csvreader)
+
+    # this will iterate through the csv file
+    for row in csvreader:
+
+            # this will incrementally count the total_votes  for each row
+            total_votes += 1
+
+            #This will extract the candidates name from the row.
+            # this will also update each candidates vote count and incrementaly count it
+            candidate_name = row[2]
+            if candidate_name not in candidates:
+                candidates[candidate_name] = 1
+            else:
+                candidates[candidate_name] += 1
+    #This will calculate the percentages and store the in a directory
+    percentages = {}
+    for candidate, votes in candidates.items():
+        percentage = (votes / total_votes) * 100
+        percentages[candidate] = percentage
+
+    # this findes the winner
+    winner = max(candidates, key=candidates.get)
+
+    #this prints the election results. incluing total number of votes for each candidate and there percentage 
+    #as compared to the others
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {total_votes}")
+    print("-------------------------")
+    for candidate, votes in candidates.items():
+        percentage = percentages[candidate]
+        print(f"{candidate}: {percentage:.3f}% ({votes})")
+    print("-------------------------")
+    print(f"Winner: {winner}")
+    print("-------------------------")
+
+    # this creates a textfile of the results from above
+    with open('election_results.txt', 'w') as textfile:
+        textfile.write("Election Results\n")
+        textfile.write("-------------------------\n")
+        textfile.write(f"Total Votes: {total_votes}\n")
+        textfile.write("-------------------------\n")
+        for candidate, votes in candidates.items():
+            percentage = percentages[candidate]
+            textfile.write(f"{candidate}: {percentage:.3f}% ({votes})\n")
+        textfile.write("-------------------------\n")
+        textfile.write(f"Winner: {winner}\n")
+        textfile.write("-------------------------\n")
